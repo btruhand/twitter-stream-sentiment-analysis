ae90bc8d91453676f4270168dc4d6dddc1de04c2
Btara Truhandarien <btruhand@sfu.ca>
2018-11-30 15:23:10 -0800

    Added more scripts for kafka topics setup

--- scripts/delete-all-kafka-topics.sh
+++ scripts/delete-all-kafka-topics.sh
@@ -7,42 +7,11 @@ CONTAINER_ZOOKEEPER_ID=$(docker ps -a -q -f "name=${DIR_NAME}_zookeeper_1")
 CONTAINER_KAFKA1_ID=$(docker ps -a -q -f "name=${DIR_NAME}_kafka1_1")
 CONTAINER_KAFKA2_ID=$(docker ps -a -q -f "name=${DIR_NAME}_kafka1_1")
 
-TOPICS=("SaluteToService"
-"SimonFraserUniversity"
-"Interesting-topics"
-"BigData"
-"noshavenovember"
-"Christmas"
-"Trudeau"
-"Canada"
-"Vancouver"
-"Midterms"
-"Trump"
-"POTUS"
-"MulledWine"
-"Snow"
-"Cold"
-"Caravan"
-"-ff"
-"-followfriday"
-"-followback"
-"-giveaway"
-"-contest"
-"-win"
-"-competition"
-"-crypto"
-"-ico"
-"-bitcoin"
-"-funny"
-"-photography"
-"-pets"
-)
-
 if [[ ! -z "$CONTAINER_KAFKA1_ID" && ! -z "$CONTAINER_KAFKA2_ID" ]]; then
-    echo "Creating topics..."
-    for topic in "${TOPICS[@]}"; do
-        docker exec "$CONTAINER_KAFKA1_ID" kafka-topics.sh --zookeeper zookeeper:2181 --create --if-not-exists --replication-factor 2 --partitions 1 --topic "${topic}"
-    done
+    echo "Deleting topics (including customer offset)..."
+    AVAILABLE_TOPICS=$(docker exec $CONTAINER_KAFKA1_ID kafka-topics.sh --zookeeper zookeeper:2181 --list)
+    TOPICS_TO_DELETE=$(echo $AVAILABLE_TOPICS | sed 's/\ /,/g')
+    docker exec "$CONTAINER_KAFKA1_ID" kafka-topics.sh --zookeeper zookeeper:2181 --delete --topic "$TOPICS_TO_DELETE"
 else
     echo >&2 "Both containers for kafka must exist but at least one of them doesn't. Please bring them up with docker-compose first"; exit 1;
 fi
