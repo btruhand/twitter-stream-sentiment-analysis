f3af762b1a00bd213223d7f853131287749ee47d
Btara Truhandarien <btruhand@sfu.ca>
2018-12-04 11:53:33 -0800

    Update testing_tweepy.py to get full text

--- misc/testing_tweepy.py
+++ misc/testing_tweepy.py
@@ -1,5 +1,4 @@
 import tweepy
-import time
 import json
 
 consumer_api_key = '2cxr421lek7AuoJZB1zMVkLDr'
@@ -11,8 +10,25 @@ fd = open('test.json', 'w')
 
 class TestListener(tweepy.StreamListener):
 	def on_status(self, status):
-		json.dump(status._json, fd)
-		fd.write("\n")
+		json_data = status._json
+		text = json_data['text']
+		is_retweeted = 'retweeted_status' in json_data
+		if is_retweeted:
+			if json_data['retweeted_status']['truncated']:
+				# there is an extended tweet
+				text = json_data['retweeted_status']['extended_tweet']['full_text']
+			else:
+				text = json_data['retweeted_status']['text']
+		else:
+			if json_data['truncated']:
+				# there is an extended tweet
+				text = json_data['extended_tweet']['full_text']
+			else:
+				text = json_data['text']
+		created_at = json_data['created_at']
+		fd.write(text.replace("\n", ' '))# json.dump(text, fd)
+		fd.write('\n')
+		# fd.write("\n")
 
 auth = tweepy.OAuthHandler(consumer_api_key, consumer_api_secret)
 auth.set_access_token(access_token, access_token_secret)
