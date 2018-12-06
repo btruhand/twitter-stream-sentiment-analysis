5417d8581e8ee82759f42faeab656b0817af2de2
Btara Truhandarien <btruhand@sfu.ca>
2018-12-05 17:55:32 -0800

    Added spark and pyspark into docker

---
+++ spark/sentiment.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession, types
+
+def main(spark):
+	print('In main')
+	schema = types.StructType([
+		types.StructField('col1', types.IntegerType()),
+		types.StructField('col2', types.IntegerType()),
+		types.StructField('col3', types.IntegerType()),
+	])
+	df = spark.createDataFrame([(1,2,3), (4,5,6)], schema)
+	df.show()
+
+if __name__ == '__main__':
+	spark = SparkSession.builder.appName('sentiment').getOrCreate()
+	assert spark.version >= '2.3'
+	main(spark)
---
+++ spark/template.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# grabbed from: https://github.com/big-data-europe/docker-spark/blob/master/template/python/template.sh
+
+sh /submit.sh
